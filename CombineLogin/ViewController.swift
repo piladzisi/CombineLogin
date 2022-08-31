75411abcf1aee5faa2f3a748adfa8b2df40ffb97
Anna Sibirtseva <a.sibirtseva@accenture.com>
2022-08-31 12:38:26 +0300

    Setup UI

---
+++ CombineLogin/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CombineLogin
+//
+//  Created by Anna Sibirtseva on 31/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var securityCode: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var repeatPassword: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
